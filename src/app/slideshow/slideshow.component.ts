febb77d4064359d10dae6b7d6632fcd721179962
LarsPeters97 <larsar.peterlov@gmail.com>
2023-05-05 19:03:59 +0200

    menu

---
+++ src/app/slideshow/slideshow.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slideshow',
+  templateUrl: './slideshow.component.html',
+  styleUrls: ['./slideshow.component.scss'],
+})
+export class SlideshowComponent implements OnInit {
+  images = ['a-i.jpg', 'laptop.jpg', 'matrix.jpg'];
+  headlines = [
+    'Bring engineering to the next level !',
+    'Born to code',
+    'Programmer and businessman',
+  ];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10);
+    }, 8000);
+  }
+}
